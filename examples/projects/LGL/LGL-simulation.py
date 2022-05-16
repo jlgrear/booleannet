aa369ee6ef529d2b800614e229b846d091922b53
jlgrear <105671951+jlgrear@users.noreply.github.com>
2022-05-16 12:27:26 -0700

    Update LGL-simulation.py

--- examples/projects/LGL/LGL-simulation.py
+++ examples/projects/LGL/LGL-simulation.py
@@ -48,7 +48,7 @@ def run( text, nodes, repeat, steps ):
 if __name__ == '__main__':
 
     # read in the text
-    text = file( examples/projects/'LGL.txt').read()
+    text = file( 'https://github.com/jlgrear/booleannet/blob/master/examples/projects/LGL/LGL.txt').read()
 
     # the nodes of interest that are collected over the run
     # NODES  = 'Apoptosis STAT3 FasL Ras'.split()
